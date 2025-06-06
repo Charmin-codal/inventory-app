dfd6cf4b87587d6aee0862a3403869801ebf838c
Charmin-SOH <charmin.patel@simpleonlinehealthcare.com>
2025-06-06 12:41:50 +0530

    add a swagger for all the apis

--- api/index.js
+++ api/index.js
@@ -8,6 +8,10 @@ const wishlistRoutes = require("../src/routes/wishlistRoutes");
 const userRoutes = require("../src/routes/userRoutes");
 
 const app = express();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('../swagger.json');
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(cors());
 app.use(express.json());
